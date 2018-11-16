4866152e75f7920bf09a47a7bdc280452f3dc81f
compupro <7752184+compupro@users.noreply.github.com>
2018-11-16 09:07:59 -0800

    Format test

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 document.getElementById("input").value = "qui fur argentarii concordes atrocis illius celeriter in atrio atrium.\n" + `Puella mea me non amat. Vale, puella! Catullus obdurat: poeta puellam non amat, puellam
 nōn vocat, formam puellae non laudat, puellae rosas non dat, et puellam non basiat! Ira mea
-est magna! Obduro, mea puella—sed sine te non valeo.
+est magna! Obduro, mea puella— sed sine te non valeo.
 
 ego sum.
 tu es.
