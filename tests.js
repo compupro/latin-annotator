073b107df08f9bb4f890713c8f5b12cdf0581d8b
compupro <7752184+compupro@users.noreply.github.com>
2018-12-18 09:33:47 -0800

    Put periods at the end of some test lines to stop leaking over

--- tests.js
+++ tests.js
@@ -2,21 +2,21 @@ document.getElementById("input").value = `Puella mea me non amat. Vale, puella! 
 nōn vocat, formam puellae non laudat, puellae rosas non dat, et puellam non basiat! Ira mea
 est magna! Obduro, mea puella— sed sine te non valeo.
 
-<NOUN/PRONOUN-VERB AGREEMENT>
+<NOUN/PRONOUN-VERB AGREEMENT>.
 Ego bibo aquam (bibit bibis).
 Nos bibimus aquam (bibunt bibitis).
 Tu bibis aquam (bibo bibit).
 Poeta bibit aquam (bibo bibis)
 
-<ADBVERB-VERB AGREEMENT>
+<ADBVERB-VERB AGREEMENT>.
 Interdum bibo bibere bibisse.
 
-<PREPOSITION-OBJECT AGREEMENT>
+<PREPOSITION-OBJECT AGREEMENT>.
 In culinis culinam.
 Apud culinam (culinis).
-Ex culinis (culinam)
+Ex culinis (culinam).
 
-<ADJECTIVE-NOUN AGREEMENT>
+<ADJECTIVE-NOUN AGREEMENT>.
 Caerulus caelus (caeli caelum).
 Caerula flora (agricola florae).`;
 processInput();
